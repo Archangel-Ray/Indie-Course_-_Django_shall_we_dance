b0446a7887205c0dc5995e572d26d1b39b8131bb
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-12-13 13:33:38 +0300

    2.3 Создаем Url и View
    внимательно пересматриваю уроки и пытаюсь показать в Ридми как
    проходил курс и что в репозитории для чего.

--- my_page/my_page/views.py
+++ my_page/my_page/views.py
@@ -1,11 +1,8 @@
 from django.http import HttpResponse
 
-directions = ["Знаки зодиака", "Расписание", "Геометрия", "Актёр", "Новостная лента",
-              "Информция о людях", "табличка"]
-project_links = ['horoscope/', 'week_days/', 'calculate_geometry/', 'actors/',
-                 'guinn/', 'people', 'beautiful_table/']
-project_directions = [f"<li><a href={link}>{direction}</a></li>"
-                      for link, direction in zip(project_links, directions)]
+directions = ["Знаки зодиака", "Расписание", "Геометрия", "Актёр", "Новостная лента", "Информация о людях", "табличка"]
+project_links = ['horoscope/', 'week_days/', 'calculate_geometry/', 'actors/', 'guinn/', 'people', 'beautiful_table/']
+project_directions = [f"<li><a href={link}>{direction}</a></li>" for link, direction in zip(project_links, directions)]
 
 
 def main(request):
