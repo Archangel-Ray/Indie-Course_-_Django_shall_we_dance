aa54268d4c2f55423abae0027c0fc3f092fa3b56
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-12-14 05:02:25 +0300

    2.7 Конвертеры роутов в Django - считать и конвертировать число

--- my_page/week_days/urls.py
+++ my_page/week_days/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.main),
     path('<YYYY:what_year>/', views.get_years_converter),
     path('<int:day_of_the_week>/', views.what_is_day_of_week_number),
-    path('<day_of_the_week>/', views.what_day_of_the_week, name="день недели"),
+    path('<str:day_of_the_week>/', views.what_day_of_the_week, name="день недели"),
 ]
