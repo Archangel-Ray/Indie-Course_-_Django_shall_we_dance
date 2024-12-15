e6186ebc5d6e257428cb32fe310fb2d23e22eadb
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-12-15 19:11:04 +0300

    2.8 Redirect Url в Django - функция redirect
    по заданию необходимо было использовать функцию, а я почему-то сделал
    это через класс. исправил. и дописал конспект.

--- my_page/week_days/views.py
+++ my_page/week_days/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.shortcuts import redirect
 from django.urls import reverse
 
 week = {
@@ -26,7 +27,7 @@ def what_day_of_the_week(request, day_of_the_week):
 def what_is_day_of_week_number(request, day_of_the_week):
     if 0 < day_of_the_week < 8:
         link = reverse("день недели", args=[list(week)[day_of_the_week - 1]])
-        return HttpResponseRedirect(link)
+        return redirect(link)
     else:
         return HttpResponseNotFound(f"Неверный номер дня недели - {day_of_the_week}")
 
