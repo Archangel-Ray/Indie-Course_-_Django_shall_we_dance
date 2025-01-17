0e6334eb590be257d6ef6a04802952b59cd7fc38
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-01-17 19:30:37 +0300

    7.3 Формы. Get и Post запросы

--- from_project/feedback/views.py
+++ from_project/feedback/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 
 def index(request):
-    print(request.GET)
+    print('ГЕТ запрос: ', request.GET)
+    print('ПОСТ запрос: ', request.POST)
     return render(request, 'feedback/feedback.html')  # шаблон с формой
