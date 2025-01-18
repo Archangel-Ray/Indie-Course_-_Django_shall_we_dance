b47db0a039f16198ef3eeadbcf1fa42c3147c0aa
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-01-18 13:53:40 +0300

    7.4 Ручная валидация формы - проверка поля "вручную"
    прописали проверку полученных данных на заполненность в представлении.
    сделали вывод сообщения об ошибке в случае если поле не заполнено.

--- from_project/feedback/views.py
+++ from_project/feedback/views.py
@@ -5,10 +5,16 @@ from django.http import HttpResponseRedirect
 def index(request):
     if request.method == 'POST':
         name = request.POST['name']
+        if not name:
+            # форма с выводом сообщения об ошибке
+            return render(request, 'feedback/feedback.html', context={'got_error': True})
         print(name)
-        return HttpResponseRedirect('/done')  # перенаправление на сообщение о подтверждении
-    return render(request, 'feedback/feedback.html')  # шаблон с формой
+        # перенаправление на сообщение о подтверждении
+        return HttpResponseRedirect('/done')
+    # шаблон с формой. сообщение об ошибке не выводить
+    return render(request, 'feedback/feedback.html', context={'got_error': False})
 
 
 def done(request):
-    return render(request, 'feedback/done.html')  # шаблон с сообщением о подтверждении
+    # шаблон с сообщением о подтверждении
+    return render(request, 'feedback/done.html')
