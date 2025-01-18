bbe2cb6d5600696147695cb50b9995641c14c781
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-01-18 13:05:48 +0300

    7.4 Ручная валидация формы - шаблон-подтверждение о получении
    добавили шаблон на который идёт перенаправление при отправке формы.

--- from_project/feedback/views.py
+++ from_project/feedback/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 
 def index(request):
-    print('ГЕТ запрос: ', request.GET)
-    print('ПОСТ запрос: ', request.POST)
+    if request.method == 'POST':
+        name = request.POST['name']
+        print(name)
+        return HttpResponseRedirect('/done')  # перенаправление на сообщение о подтверждении
     return render(request, 'feedback/feedback.html')  # шаблон с формой
+
+
+def done(request):
+    return render(request, 'feedback/done.html')  # шаблон с сообщением о подтверждении
