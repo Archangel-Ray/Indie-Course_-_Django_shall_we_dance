ff86d27e0a0a469bebeffc8e1acf0a1ec3962d2a
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-01-22 18:55:52 +0300

    7.9 ModelForm - настройка минимальной длины поля (в модели)

--- from_project/feedback/models.py
+++ from_project/feedback/models.py
@@ -1,8 +1,9 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
 
 class Feedback(models.Model):
-    name = models.CharField(max_length=40)
+    name = models.CharField(max_length=40, validators=[MinLengthValidator(5)])
     surname = models.CharField(max_length=60)
     feedback = models.TextField()
     rating = models.PositiveIntegerField()
