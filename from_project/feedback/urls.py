91adca59fb535d2975400ff4ac4a965295ac212d
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-01-25 15:02:25 +0300

    7.11 Class Based Views - запуск класса представления

--- from_project/feedback/urls.py
+++ from_project/feedback/urls.py
@@ -1,10 +1,10 @@
 # from django.contrib import admin
 from django.urls import path
 
-from .views import index, done, update_feedback
+from .views import index, done, update_feedback, FeedBackView
 
 urlpatterns = [
     path('done', done),  # подтверждение о получении формы
-    path('', index),  # запуск представления с формой
+    path('', FeedBackView.as_view()),  # запуск представления с формой
     path('<int:id_feedback>', update_feedback),  # запуск представления с указанной записью из базы
 ]
