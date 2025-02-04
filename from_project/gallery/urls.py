a24693982cef8992185fc440e84c7d1ab3fb806a
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-02-04 13:01:01 +0300

    9.4 Отображение файлов в шаблоне - подключение роутов считывания медиа

--- from_project/gallery/urls.py
+++ from_project/gallery/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -7,4 +9,4 @@ urlpatterns = [
     path('load_image', views.GalleryView.as_view()),
     path('create_load_image', views.CreateGalleryView.as_view()),
     path('list_image', views.ListGallery.as_view()),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
