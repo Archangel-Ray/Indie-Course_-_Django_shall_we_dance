1324616868054340f732028213cebb95728944fe
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-02-04 13:11:34 +0300

    сделал навигацию

--- from_project/gallery/urls.py
+++ from_project/gallery/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns = [
     # форма загрузки файла
-    path('load_image', views.GalleryView.as_view()),
-    path('create_load_image', views.CreateGalleryView.as_view()),
-    path('list_image', views.ListGallery.as_view()),
+    path('load_image', views.GalleryView.as_view(), name="загрузка файла в ручную"),
+    path('create_load_image', views.CreateGalleryView.as_view(), name="загрузка файла автоматически"),
+    path('list_image', views.ListGallery.as_view(), name="вывод всех файлов"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
