2dcd9c41f1a23e6bd793385d3adf578eda8ceeaa
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-02-04 14:28:41 +0300

    перенёс подключение роута для статики в urls проекта
    немножко был не внимательный. не обратил внимание, что статику надо
    подключать ко всему проекту, а не к приложению.

--- from_project/gallery/urls.py
+++ from_project/gallery/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 
 from . import views
 
@@ -9,4 +7,4 @@ urlpatterns = [
     path('load_image', views.GalleryView.as_view(), name="загрузка файла в ручную"),
     path('create_load_image', views.CreateGalleryView.as_view(), name="загрузка файла автоматически"),
     path('list_image', views.ListGallery.as_view(), name="вывод всех файлов"),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
